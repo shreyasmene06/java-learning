bf41c01d011679b74c9855b81094b94e06086c16
shreyasmene06 <shreyasmene6@gmail.com>
2024-05-26 02:54:54 +0530

    commit

---
+++ lec2prac.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class lec2prac {
+    public static void main(String[] args){
+        Scanner sc = new Scanner (System.in);
+        String name=sc.next();
+        System.out.println(name);
+        //nextint 
+        //nextfloat
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.err.println(a+b);
+        //program to print sum
+        sc.close();
+       }
+    
+    
+}
